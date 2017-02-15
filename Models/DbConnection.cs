5524605c0525de6fef290d06c1a4f92e4e24807c
King <gxnews8@gmail.com>
2017-02-15 12:14:22 -0800

    add DbConnection.cs

---
+++ Models/DbConnection.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using System.Data.Common;
+using MySql.Data.MySqlClient;
+
+namespace fightingGame
+{
+    public class DbConnector
+    {
+        public static List<Dictionary<string, object>> ExecuteQuery(string queryString)
+        {
+            string server = "localhost";
+            string db = "fightingGame";
+            string port = "3306";
+            string user = "root";
+            string pass = "root";
+            using(var connection = new MySqlConnection(
+                $"Server={server};Database={db};Port={port};UserID={user};Password={pass};"))
+            {
+                connection.Open();
+                using(var command = new MySqlCommand(queryString, connection))
+                {
+                    var result = new List<Dictionary<string, object>>();
+                    using(DbDataReader rdr = command.ExecuteReader())
+                    {
+                        while(rdr.Read())
+                        {
+                            var dict = new Dictionary<string, object>();
+                            for( int i = 0; i < rdr.FieldCount; i++ ) {
+                                dict.Add(rdr.GetName(i), rdr.GetValue(i));
+                            }
+                            result.Add(dict);
+                        }
+                    }
+                    return result;
+                }
+            }
+        }
+    }
+}
